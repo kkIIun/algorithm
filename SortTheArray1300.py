aecf7a7bb1dad658ec12ee25bc5b7bd712fcf201
leekijung <rlwjd4177@naver.com>
2021-04-01 22:22:08 +0900

    Sort The Array

--- SortTheArray1300.py
+++ SortTheArray1300.py
@@ -3,24 +3,23 @@ array = [int(x) for x in input().split()]
 
 Carray = array[:]
 Carray.sort()
-first, seccond, fleg = 1, 1, 0
+first, seccond, fleg = 0, 0, 0
 for i in range(len(array) - 1):
-    if array[i] > array[i + 1]:
+    if array[i] > array[i + 1] and fleg == 0:
         first = i
-    elif array[first] < array[i + 1] and fleg == 0:
-        seccond = i
         fleg = 1
 
-i = 0
-for item in array[1:-1:-1]:
-    array[i] = item
-    i += 1
-    print(item)
+    elif array[first] < array[i + 1] and fleg == 1:
+        seccond = i
+        fleg += 1
 
-print(array)
+if fleg and seccond == 0:
+    seccond = len(array) - 1
+array = array[:first] + array[first : seccond + 1][::-1] + array[seccond + 1 :]
+
 if Carray != array:
     print("no")
 
 else:
     print("yes")
-    print(first, seccond)
+    print(first + 1, seccond + 1)
