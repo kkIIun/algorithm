e281528b5504a7d6b7b3e1ea14e6297b979aca3a
leekijung <rlwjd4177@naver.com>
2021-04-03 19:49:27 +0900

    success

--- 478B-RandomTeams.py
+++ 478B-RandomTeams.py
@@ -1,11 +1,9 @@
 def freinds_pair(num):
-    return int(num * (num - 1) / 2)
+    return int(num * (num - 1) // 2)
 
 
 n, m = map(int, input().split())
 
 a, b = n // m, n % m
 Max, Min = freinds_pair(n - m + 1), freinds_pair(a) * (m - b) + freinds_pair(a + 1) * (b)
-if Max > pow(10, 9):
-    Max = round(Max, -3)
 print(Min, Max)
