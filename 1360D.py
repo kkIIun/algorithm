5d6374a17929cbb3f4073e0feb03fa67630af111
kkIIun <rlwjd4177@naver.com>
2021-04-09 01:19:37 +0900

    1360D/Buying Shovels

--- 1360D.py
+++ 1360D.py
@@ -1,4 +1,19 @@
+import math
+
 t = int(input())
 
 for _ in range(t):
     n, k = map(int, input().split())
+    if k >= n:
+        print(1)
+
+    else:
+        ans = n
+        for i in range(2, int(math.sqrt(n)) + 1):
+            if n % i == 0:
+                if i <= k:
+                    ans = min(ans, n // i)
+                elif n // i <= k:
+                    ans = min(ans, i)
+
+        print(ans)
