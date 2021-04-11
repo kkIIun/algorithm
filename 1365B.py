34769421f997673ca42b77ed10060fca3251e53b
kkIIun <rlwjd4177@naver.com>
2021-04-11 14:52:41 +0900

    1365B/ Trouble Sort

---
+++ 1365B.py
@@ -0,0 +1,20 @@
+t = int(input())
+
+for _ in range(t):
+    n = input()
+    a = [int(x) for x in input().split()]
+    b = [int(x) for x in input().split()]
+
+    o, z = 0, 0
+    for i in range(len(a)):
+        if b[i]:
+            o += 1
+        else:
+            z += 1
+
+    Ca = a[:]
+    Ca.sort()
+    if a == Ca or (o > 0 and z > 0):
+        print("YES")
+    else:
+        print("NO")
