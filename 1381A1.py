5de4d1880d21d49b602d5ad0e35632c1ba3e2eae
kkIIun <rlwjd4177@naver.com>
2021-05-01 00:56:46 +0900

    1381A1/ Prefix Flip(Esay Version)

---
+++ 1381A1.py
@@ -0,0 +1,34 @@
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    a = list(map(int, input()))
+    b = list(map(int, input()))
+
+    ans = 0
+    string = ""
+    k = len(a) - 1
+    while a != b:
+        for i in range(k, 0 - 1, -1):
+            if a[i] != b[i]:
+                k = i
+                break
+
+        if a[0] == b[k]:
+            ans += 1
+            if a[0] == 0:
+                a[0] = 1
+            else:
+                a[0] = 0
+            string += "1 "
+
+        ans += 1
+        string += str(k + 1) + " "
+        for i in range(k + 1):
+            if a[i] == 0:
+                a[i] = 1
+            else:
+                a[i] = 0
+        a = a[: k + 1][::-1] + a[k + 1 :]
+
+    print(str(ans) + " " + string)
