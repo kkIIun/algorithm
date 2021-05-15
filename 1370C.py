3b829a291cd3442ad48242c9f2c34adf8391ec19
kkIIun <rlwjd4177@naver.com>
2021-05-15 23:57:52 +0900

    Number Game

--- 1370C.py
+++ 1370C.py
@@ -1,8 +1,39 @@
+import math
+
+
+def isprime(n):
+    for i in range(2, int(math.pow(n, 0.5)) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
 t = int(input())
 
 for _ in range(t):
+    ans = 1  # odd FastestFinger, even Ashishgup win
     n = int(input())
-
-
-
-    print(ans)
+    while n != 1:
+        if n % 2 != 0:
+            n //= n
+        else:
+            copy = n
+            div = n
+            while div % 2 == 0:
+                div //= 2
+            while not isprime(div):
+                if n // div > 2:
+                    n //= div
+                    break
+                else:
+                    for i in range(3, div + 1):
+                        if div % i == 0:
+                            div //= i
+                            break
+            if n == copy:
+                n -= 1
+        ans += 1
+    if ans % 2 == 0:
+        print("Ashishgup")
+    else:
+        print("FastestFinger")
