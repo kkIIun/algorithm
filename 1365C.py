41e33c077173df44cab609257ce4ce5f4077a4dd
kkIIun <rlwjd4177@naver.com>
2021-05-20 00:21:40 +0900

    1365C/ Rotation Matching

--- 1365C.py
+++ 1365C.py
@@ -1,1 +1,25 @@
-input()
+n = int(input())
+
+a = [int(x) for x in input().split()]
+b = [int(x) for x in input().split()]
+
+pos = [0] * (n + 1)
+for i in range(n):
+    pos[a[i]] = i
+
+for i in range(n):
+    pos[b[i]] -= i
+    if pos[b[i]] < 0:
+        pos[b[i]] += n
+
+counter = {}
+for i in range(1, n + 1):
+    if pos[i] in counter:
+        counter[pos[i]] += 1
+    else:
+        counter[pos[i]] = 1
+
+ans = 0
+for i, v in counter.items():
+    ans = max(ans, v)
+print(ans)
