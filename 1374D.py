45ea618b6a045de764d892e6a4d497d9e9396275
kkIIun <rlwjd4177@naver.com>
2021-05-24 20:03:03 +0900

    1374D/ Zero Remainder Array

---
+++ 1374D.py
@@ -0,0 +1,25 @@
+import math
+
+t = int(input())
+
+for _ in range(t):
+    n, k = map(int, input().split())
+    a = [int(x) for x in input().split()]
+    a.sort()
+
+    ans = 0
+    while a:
+        l, r = 0, len(a) - 1
+        while l <= r:
+            m = round((l + r) / 2)
+            if a[m] == (k - ans) + k * (ans // k):
+                a.pop(m)
+                break
+            elif a[m] > (k - ans) + k * (ans // k):
+                r -= 1
+            else:
+                l += 1
+
+        ans += 1
+
+    print(ans)
