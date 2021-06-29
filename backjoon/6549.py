e12f5993b5d5835366cab88f4c462c1ae3741aa4
kkIIun <rlwjd4177@naver.com>
2021-06-29 18:13:28 +0900

    6549: fail segmentTree(recursive error)

--- backjoon/6549.py
+++ backjoon/6549.py
@@ -1,40 +1,64 @@
 import math
 
 
-def minIndex(arr):
-    Min = 9999999
-    index = 0
-    for i in range(len(arr)):
-        if arr[i] < Min:
-            Min = arr[i]
-            index = i
-    return index
+def query(arr, tree, node, start, end, left, right):
+    if end < left or start > right:
+        return -1
 
-
-def segmentTree(arr, tree, node, start, end):
-    if not (arr):
-        return 0
-
-    if start == end:
-        tree[node] = arr.pop()
+    if end <= right and start >= left:
         return tree[node]
 
-    mid = minIndex(arr)
-    tree[node] = arr[mid] * len(arr)
+    mid = (start + end) // 2
+    m1 = query(arr, tree, node * 2, start, mid, left, right)
+    m2 = query(arr, tree, node * 2 + 1, mid + 1, end, left, right)
 
-    tree[node] = max(
-        segmentTree(arr[:mid], tree, node * 2, start, mid - 1),
-        segmentTree(arr[mid + 1 :], tree, node * 2 + 1, mid + 1, end),
-        tree[node],
-    )
-    return tree[node]
+    if m1 == -1:
+        return m2
+
+    elif m2 == -1:
+        return m1
+
+    else:
+        if arr[m1] <= arr[m2]:
+            return m1
+        else:
+            return m2
+
+
+def init(arr, tree, node, start, end):
+    if start == end:
+        tree[node] = start
+    else:
+        mid = (start + end) // 2
+        init(arr, tree, node * 2, start, mid)
+        init(arr, tree, node * 2 + 1, mid + 1, end)
+
+        if arr[tree[node * 2]] > arr[tree[node * 2 + 1]]:
+            tree[node] = tree[node * 2 + 1]
+        else:
+            tree[node] = tree[node * 2]
+
+
+def getMax(arr, tree, start, end):
+    mid = query(arr, tree, 1, 0, len(arr) - 1, start, end)
+
+    area = arr[mid] * (end - start + 1)
+    if start <= mid - 1:
+        area = max(area, getMax(arr, tree, start, mid - 1))
+
+    if end >= mid + 1:
+        area = max(area, getMax(arr, tree, mid + 1, end))
+
+    return area
 
 
 while 1:
     h = [int(x) for x in input().split()]
-    l = math.ceil(math.log2(h[0]))
-    tree = [None] * (1 << (l + 2))
-    print(len(tree))
-    if h.pop(0) == 0:
+    n = h.pop(0)
+    if n == 0:
         break
-    print(segmentTree(h, tree, 1, 0, len(h) - 1))
+    l = math.ceil(math.log2(n))
+    tree = [None] * (1 << l + 1)
+
+    init(h, tree, 1, 0, n - 1)
+    print(getMax(h, tree, 0, n - 1))
