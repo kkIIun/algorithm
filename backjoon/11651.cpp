716754d7691642b3404c019c1c7c853c18aee387
kkIIun <rlwjd4177@naver.com>
2021-09-10 23:21:19 +0900

    1450: 이분 탐색 어려움

---
+++ backjoon/11651.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll n;
+vector<pair<ll, ll>> v;
+
+void solve()
+{
+    cin >> n;
+    ll x, y;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> x >> y;
+        v.push_back(make_pair(y, x));
+    }
+    sort(v.begin(), v.end());
+
+    for (ll i = 0; i < n; i++)
+        cout << v[i].second << ' ' << v[i].first << '\n';
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
---
+++ backjoon/1450.cpp
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+ll n, c, ans, visited[32768];
+vector<ll> v;
+
+vector<ll> splitArr(ll left, ll len)
+{
+    vector<ll> result;
+    for (ll i = 0; i < (1 << len); i++)
+    {
+        ll sum = 0;
+        for (ll j = 0; j < len; j++)
+            if ((1 << j) & i)
+                sum += v[left + j];
+        result.push_back(sum);
+    }
+    return result;
+}
+
+void solve()
+{
+    cin >> n >> c;
+    v.assign(n, 0);
+    for (ll i = 0; i < n; i++)
+        cin >> v[i];
+    vector<ll> g1 = splitArr(0, n / 2);
+    vector<ll> g2 = splitArr(n / 2, ceil(n / 2.0));
+    sort(g2.begin(), g2.end());
+    for (ll i = 0; i < g1.size(); i++)
+    {
+        ll left = 0, right = g2.size() - 1;
+        while (left < right)
+        {
+            ll mid = ceil((left + right) / 2.0);
+            if (c - g1[i] >= g2[mid])
+                left = mid;
+            else
+                right = mid - 1;
+        }
+        ans += left + 1;
+        // ans += upper_bound(g2.begin(), g2.end(), c - g1[i]) - g2.begin();
+    }
+    cout << ans << endl;
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    solve();
+    return 0;
+}
