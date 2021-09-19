7cb07a8707d77ce87d2638b4a2bba2a9cb3d461b
kkIIun <rlwjd4177@naver.com>
2021-09-20 00:44:44 +0900

    14725: trie구조

--- backjoon/14725.cpp
+++ backjoon/14725.cpp
@@ -5,44 +5,62 @@ typedef long long ll;
 
 ll n, k;
 
-// struct node
-// {
-//     string data;
-//     ll level;
-//     vector<node> childNode;
-// };
+struct node
+{
+    string data;
+    map<string, node *> mp;
+    node() { data = "", mp.clear(); }
+};
 
-// bool cmp(node a, node b)
-// {
-//     return a.data < b.data;
-// }
+class Trie
+{
+public:
+    node *root;
+    Trie() { root = new node(); }
 
-// void set(node prev, ll level)
-// {
-//     node temp;
-//     cin >> temp.data;
-//     prev.
-// }
+    void set(node *current, string s)
+    {
+        if (current->mp[s] == nullptr)
+            current->mp[s] = new node();
+
+        current->mp[s]->data = s;
+    }
+
+    void find(node *current, ll depth)
+    {
+        if (current == nullptr)
+            return;
+
+        for (ll i = 1; i < depth; i++)
+            cout << "--";
+
+        if (current != root)
+            cout << current->data << '\n';
+
+        for (auto next : current->mp)
+            find(next.second, depth + 1);
+    }
+};
 
 int main(void)
 {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    int *a, b = 1;
-    a = &b;
-    cout << a << '\n';
-    cout << *a << '\n';
-    // ll n;
-    // node root;
-    // cin >> n;
-    // while (n--)
-    // {
-    //     cin >> k;
-    //     set(tree, k);
-    // }
-    // node top;
-    // node temp;
-    // sort(top.childNode.begin(), top.childNode.end(), cmp);
+    cin >> n;
+    Trie trie;
+    while (n--)
+    {
+        cin >> k;
+        node *current = trie.root;
+        string in;
+        while (k--)
+        {
+            cin >> in;
+            trie.set(current, in);
+            current = current->mp[in];
+        }
+    }
+    trie.find(trie.root, 0);
     return 0;
 }
