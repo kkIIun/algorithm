77b4245a0a3c58d4d05342cbd7bfebae1ce2031f
kkIIun <rlwjd4177@naver.com>
2021-09-22 00:43:33 +0900

    3665: 위상 정렬 수정

--- backjoon/3665.cpp
+++ backjoon/3665.cpp
@@ -1,127 +1,96 @@
-#include <iostream>
-#include <vector>
-#include <queue>
+#include <bits/stdc++.h>
+
 using namespace std;
-int indegree[501];
-int g[501][501];
-int a[501];
+typedef long long ll;
+
+ll n, m;
+vector<vector<ll>> graph;
+vector<ll> score;
+vector<ll> indegree;
+
+void bfs()
+{
+    vector<ll> ans;
+    queue<ll> q;
+
+    for (ll i = 1; i <= n; i++)
+        if (indegree[i] == 0)
+            q.push(i);
+
+    while (!q.empty())
+    {
+        ll cur = q.front();
+        ans.push_back(cur);
+        q.pop();
+        for (ll i = 1; i <= n; i++)
+            if (graph[cur][i])
+                if (--indegree[i] == 0)
+                    q.push(i);
+    }
+
+    if (ans.size() == n)
+        for (ll i = 0; i < n; i++)
+            cout << ans[i] << ' ';
+    else
+        cout << "IMPOSSIBLE";
+    cout << '\n';
+}
+
 void init()
 {
-    for (int i = 0; i < 501; i++)
-    {
-        a[i] = indegree[i] = 0;
-        for (int j = 0; j < 501; j++)
+    for (ll i = 1; i <= n; i++)
+        cin >> score[i];
+
+    for (ll i = 1; i <= n; i++)
+        for (ll j = 1; j < i; j++)
         {
-            g[i][j] = 0;
+            graph[score[j]][score[i]] = 1;
+            indegree[score[i]]++;
         }
-    }
 }
-int main()
+
+void solve()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-    int tc;
-    cin >> tc;
-    for (int t = 1; t <= tc; t++)
+    cin >> n;
+    graph.assign(n + 1, vector<ll>(n + 1, 0));
+    indegree.assign(n + 1, 0);
+    score.assign(n + 1, 0);
+
+    init();
+
+    cin >> m;
+    ll a, b;
+    while (m--)
     {
-        int n, m, F = -1;
-        cin >> n;
-        init();
-        for (int i = 1; i <= n; i++)
+        cin >> a >> b;
+        if (!graph[a][b])
         {
-            cin >> a[i];
-        }
-
-        for (int i = 1; i < n; i++)
-        {
-            for (int j = i + 1; j <= n; j++)
-            {
-                g[a[i]][a[j]] = 1;
-                indegree[a[j]]++;
-            }
-        }
-
-        cin >> m;
-
-        for (int i = 0; i < m; i++)
-        {
-            int x, y;
-            cin >> x >> y;
-
-            if (g[x][y] == 1)
-            {
-                indegree[x]++;
-                indegree[y]--;
-                swap(g[x][y], g[y][x]);
-            }
-            else if (g[y][x] == 1)
-            {
-                indegree[y]++;
-                indegree[x]--;
-                swap(g[x][y], g[y][x]);
-            }
-        }
-
-        queue<int> q;
-        queue<int> ans;
-
-        for (int i = 1; i <= n; i++)
-        {
-            if (indegree[i] == 0)
-            {
-                q.push(i);
-                ans.push(i);
-            }
-        }
-
-        for (int i = 0; i < n; i++)
-        {
-            if (q.size() == 0)
-            {
-                F = 0;
-                break;
-            }
-
-            if (q.size() >= 2)
-            {
-                F = 1;
-                break;
-            }
-
-            int here = q.front();
-            q.pop();
-
-            for (int i = 1; i <= n; i++)
-            {
-                if (g[here][i] == 1)
-                {
-                    indegree[i]--;
-                    if (indegree[i] == 0)
-                    {
-                        q.push(i);
-                        ans.push(i);
-                    }
-                }
-            }
-        }
-
-        if (F == 0)
-        {
-            cout << "IMPOSSIBLE\n";
-        }
-        else if (F == 1)
-        {
-            cout << "?\n";
+            indegree[a]--;
+            indegree[b]++;
+            graph[b][a] = 0;
+            graph[a][b] = 1;
         }
         else
         {
-            while (!ans.empty())
-            {
-                cout << ans.front() << " ";
-                ans.pop();
-            }
-            cout << "\n";
+            indegree[a]++;
+            indegree[b]--;
+            graph[b][a] = 1;
+            graph[a][b] = 0;
         }
     }
+
+    bfs();
+}
+
+int main(void)
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    ll TC;
+    cin >> TC;
+    while (TC--)
+        solve();
+
     return 0;
 }
