6d3fa4bf8c9a2f80f1c8fac5c9edad8035bc8e6f
kkIIun <rlwjd4177@naver.com>
2021-09-27 14:34:45 +0900

    13511: 희소 배열, 가장 가까운 조상

--- backjoon/13511.cpp
+++ backjoon/13511.cpp
@@ -3,55 +3,92 @@
 using namespace std;
 typedef long long ll;
 
-ll n, m, start, End, visited[100001];
-vector<ll> path;
-vector<vector<pair<ll, ll>>> tree;
+ll n, m, parent[100001][20], depth[100001], cost[100001];
+vector<pair<ll, ll>> v[100001];
 
-ll dfs(ll cur, ll sum)
+void setTree(ll start)
 {
-    if (cur == End)
-        return sum;
-    ll temp;
-    for (auto next : tree[cur])
-        if (!visited[next.first])
-        {
-            visited[next.first] = 1;
-            path.push_back(next.first);
-            temp = dfs(next.first, sum + next.second);
-            if (temp)
-                return temp;
-            path.pop_back();
-        }
+    for (auto next : v[start])
+    {
+        if (parent[start][0] == next.first)
+            continue;
+        depth[next.first] = depth[start] + 1;
+        cost[next.first] = next.second + cost[start];
+        parent[next.first][0] = start;
+        setTree(next.first);
+    }
+}
 
-    return 0;
+void setParent()
+{
+    for (ll i = 1; i < 20; i++)
+        for (ll j = 1; j <= n; j++)
+            parent[j][i] = parent[parent[j][i - 1]][i - 1];
 }
 
 void solve()
 {
-    ll u, v, w;
+    ll a, b, c, t;
     cin >> n;
-    tree.assign(n + 1, vector<pair<ll, ll>>());
     for (ll i = 1; i < n; i++)
     {
-        cin >> u >> v >> w;
-        tree[u].push_back(make_pair(v, w));
-        tree[v].push_back(make_pair(u, w));
+        cin >> a >> b >> c;
+        v[a].push_back(make_pair(b, c));
+        v[b].push_back(make_pair(a, c));
     }
+    setTree(1);
+    setParent();
+
     cin >> m;
     while (m--)
     {
-        path.clear();
-        memset(visited, 0, sizeof(visited));
-        cin >> n >> start >> End;
-        visited[start] = 1;
-        path.push_back(start);
-        if (n == 1)
-            cout << dfs(start, 0) << '\n';
-        if (n == 2)
+        cin >> t >> a >> b;
+        ll tempA = a, tempB = b;
+
+        while (depth[tempA] != depth[tempB])
         {
-            dfs(start, 0);
-            cin >> w;
-            cout << path[w - 1] << '\n';
+            if (depth[tempA] < depth[tempB])
+                swap(tempA, tempB);
+            ll temp = depth[tempA] - depth[tempB];
+            tempA = parent[tempA][(ll)log2(temp)];
+        }
+
+        while (tempA != tempB)
+        {
+            for (ll i = 19; i >= 0; i--)
+                if (parent[tempA][i] != parent[tempB][i])
+                {
+                    tempA = parent[tempA][i];
+                    tempB = parent[tempB][i];
+                }
+            tempA = parent[tempA][0];
+            tempB = parent[tempB][0];
+        }
+
+        if (t == 1)
+            cout << cost[a] + cost[b] - 2 * cost[tempA] << '\n';
+
+        if (t == 2)
+        {
+            ll temp, left;
+            cin >> temp;
+            left = depth[a] - depth[tempA];
+            if (temp <= left)
+            {
+                temp = depth[tempA] + left - temp + 1;
+                while (depth[a] != temp)
+                    a = parent[a][(ll)log2(depth[a] - temp)];
+                cout << a << '\n';
+            }
+            else if (temp == left + 1)
+                cout << tempA << '\n';
+            else
+            {
+                temp -= left + 1;
+                while (temp != depth[b] - depth[tempA])
+                    b = parent[b][(ll)log2(depth[b] - temp - depth[tempA])];
+                cout << b << '\n';
+            }
         }
     }
 }
