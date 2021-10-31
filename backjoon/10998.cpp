c014c50dc56b1cacec2b7a44a2a74e870a194183
kkIIun <rlwjd4177@naver.com>
2021-10-31 23:40:00 +0900

    10998: 곱셈

---
+++ backjoon/10998.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+typedef long long ll;
+
+int main(void)
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    ll a, b;
+    cin >> a >> b;
+    cout << a * b;
+    return 0;
+}
