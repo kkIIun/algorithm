e72293ddba225b4d5431a7e6205d3317e5b16c3d
kkIIun <rlwjd4177@naver.com>
2021-11-14 00:25:33 +0900

    15683: 백트레킹

--- backjoon/15683.cpp
+++ backjoon/15683.cpp
@@ -10,17 +10,6 @@ ll dx[4] = {-1, 0, 1, 0};
 ll dy[4] = {0, 1, 0, -1};
 ll row, col;
 
-void print2d(vector<vector<ll>> map)
-{
-    for (ll i = 0; i < row; i++)
-    {
-        for (ll j = 0; j < col; j++)
-            cout << map[i][j];
-        cout << '\n';
-    }
-    cout << '\n';
-}
-
 ll move(ll x, ll y, ll dir, vector<vector<ll>> &map)
 {
     ll result = 0;
@@ -28,26 +17,27 @@ ll move(ll x, ll y, ll dir, vector<vector<ll>> &map)
     {
         x += dx[dir];
         y += dy[dir];
-        if (x < 0 || x >= col || y < 0 || y >= row || map[y][x] != 0)
+        if (x < 0 || x >= col || y < 0 || y >= row || map[y][x] == 6)
             break;
-        map[y][x] = 6;
-        result++;
+        if (!map[y][x])
+        {
+            map[y][x] = -1;
+            result++;
+        }
     }
-
     return result;
 }
 
 ll solve(vector<vector<ll>> &map, ll idx)
 {
-    print2d(map);
+
     if (idx >= cctv.size())
         return 0;
-    ll x = cctv[idx].first;
-    ll y = cctv[idx].second;
-    cout << map[x][y] << '\n';
+    ll x = cctv[idx].second;
+    ll y = cctv[idx].first;
     ll result = 0;
 
-    if (map[x][y] == 1)
+    if (map[y][x] == 1)
     {
         for (ll i = 0; i < 4; i++)
         {
@@ -56,18 +46,18 @@ ll solve(vector<vector<ll>> &map, ll idx)
             result = max(result, move(x, y, i, v) + solve(v, idx + 1));
         }
     }
-    else if (map[x][y] == 2)
+    else if (map[y][x] == 2)
     {
         for (ll i = 0; i < 2; i++)
         {
             vector<vector<ll>> v(map.size(), vector<ll>(map[0].size(), 0));
             copy(map.begin(), map.end(), v.begin());
             ll cnt = 0;
-            cnt += move(x, y, i + 2, v) + move(x, y, i, v);
+            cnt += move(x, y, i, v) + move(x, y, i + 2, v);
             result = max(result, cnt + solve(v, idx + 1));
         }
     }
-    else if (map[x][y] == 3)
+    else if (map[y][x] == 3)
     {
         for (ll i = 0; i < 4; i++)
         {
@@ -78,7 +68,7 @@ ll solve(vector<vector<ll>> &map, ll idx)
             result = max(result, cnt + solve(v, idx + 1));
         }
     }
-    else if (map[x][y] == 4)
+    else if (map[y][x] == 4)
     {
         for (ll i = 0; i < 4; i++)
         {
@@ -89,7 +79,7 @@ ll solve(vector<vector<ll>> &map, ll idx)
             result = max(result, cnt + solve(v, idx + 1));
         }
     }
-    else if (map[x][y] == 5)
+    else if (map[y][x] == 5)
     {
         ll cnt = 0;
         vector<vector<ll>> v(map.size(), vector<ll>(map[0].size(), 0));
